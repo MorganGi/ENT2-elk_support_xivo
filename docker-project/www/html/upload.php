f41cac9d0dc931ce82ef8c54830ac22f494279d4
MorganGi <91611360+MorganGi@users.noreply.github.com>
2022-04-20 17:20:06 +0200

    Add files via upload

--- docker-project/www/html/upload.php
+++ docker-project/www/html/upload.php
@@ -12,12 +12,18 @@ $where_to_upload= str_replace(" ", "", $where_to_upload);
 
 $password1= $_POST['password'];
 
-$password2= "a";//change this to the password you want 
+$password2= "a"; //change this to the password you want 
+
+if (isset($_POST['logtyp'])){   
+  $logtype= $_POST['logtyp'];
+}
+else{
+    echo "Le type du fichier n'as pas été selectionné";
+}
 
 $length= strlen($where_to_upload);
 
 $last= substr($where_to_upload, $length-1, 1);
-
 if ($last !== "/"){
 $where_to_upload= "$where_to_upload" . "/";
 }
@@ -27,27 +33,26 @@ if ($first == "/"){
 $where_to_upload= str_replace("/", "", $where_to_upload);
 }
 
+$name = "$logtype" . "-" . "$name";
 
 if (isset($name)) {
+    if (empty($name))
+        {
+        echo "Please choose a file";
+        }
+    else if ((!empty($name)) && ($password1 == $password2))
+        {
+            
+        if (move_uploaded_file($tmp_name, $where_to_upload . $name)) {
+        echo "Uploaded!";
+        }
+        }
+    else
+    {
+    echo "The password entered is incorrect. The upload has not been done.";
+    }
+}
 
-if (empty($name))
-{
-echo "Please choose a file";
-}
-else if ((!empty($name)) && ($password1 == $password2))
-{
-if (move_uploaded_file($tmp_name, $where_to_upload . $name)) {
-echo "Uploaded!";
-}
-}
-else
-{
-echo "The password entered is incorrect. The upload has not been done.";
-}
-}
-
-?>
-<?php
 if  ((!empty($name)) && ($password1 == $password2)){
 echo "The file you uploaded is shown below.<br><br>";
 echo "<a href='$where_to_upload" ."$name'>$name</a>";
