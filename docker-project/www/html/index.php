df95b16f1b8eb6e29ac1ba8cbf0078c9a6e959c3
MorganGi <91611360+MorganGi@users.noreply.github.com>
2022-04-21 15:17:43 +0200

    Add files via upload

--- docker-project/www/html/index.php
+++ docker-project/www/html/index.php
@@ -1,4 +1,24 @@
 
+<style>
+input[type=text], select {
+  width: 25%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+input[type=button], input[type=submit], input[type=reset] {
+  background-color: #04AA6D;
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-decoration: none;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
 
 <form action="upload.php" method='post' enctype="multipart/form-data">
 <input type="file" name="file"/><br><br>
